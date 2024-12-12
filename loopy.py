594a82ea94c0e2af55ee34170fa2367e448ecd74
Tom Brandenburg <t_bh@gmx.de>
2024-12-12 21:49:00 +0100

    Changed to curses display

--- loopy.py
+++ loopy.py
@@ -1,9 +1,11 @@
 import mido
 import threading
+import curses
 import time
 import fluidsynth
 import sys
 import logging
+import traceback
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -55,7 +57,7 @@ class InstrumentRegistry:
 
         channel_info = self._fs.channel_info(available_channel)
 
-        logging.info(f"Registered '{channel_info[3]}' (bank={bank},preset={preset}) for FluidSynth channel {available_channel} and mapped to '{instrument_name}'!")
+        logging.debug(f"Registered '{channel_info[3]}' (bank={bank},preset={preset}) for FluidSynth channel {available_channel} and mapped to '{instrument_name}'!")
 
     def get_instrument(self, instrument_name):
         """Returns the FluidSynth instance and the instrument's channel."""
@@ -71,10 +73,10 @@ class InstrumentRegistry:
 class InstrumentChannel:
     """Abstract base class for all instrument channels."""
     
-    def __init__(self, project, instrument_name, volume=80):
-        self._project = project
+    def __init__(self, parent, instrument_name, volume=80):
+        self._parent = parent
         self._volume = volume
-        self._instrument_registry = project.get_instrument_registry()
+        self._instrument_registry = parent.get_instrument_registry()
         self.set_instrument(instrument_name)
         self._is_playing = False
 
@@ -90,17 +92,21 @@ class InstrumentChannel:
     def get_volume(self):
         """Returns the channel's volume."""
         return self._volume
+    
+    def get_parent(self):
+        """Returns parent"""
+        return self._parent
 
     def play(self):
         """Starts the channel."""
         if not self._is_playing:
-            logging.info(f"Starting channel '{self}'...")
+            logging.debug(f"Starting channel '{self}'...")
             self._is_playing = True
 
     def stop(self):
         """Stops the channel."""
         if self._is_playing:
-            logging.info(f"Pausing channel '{self}'...")
+            logging.debug(f"Pausing channel '{self}'...")
             self._is_playing = False
 
     def tick(self):
@@ -119,14 +125,14 @@ class FreeMidiChannel(InstrumentChannel):
         """Starts the channel and begins receiving MIDI data."""
         super().play()  # Set the channel to "play"
         if self._is_playing:
-            logging.info(f"Begin receiving MIDI data on channel {self._instrument_name}...")
+            logging.debug(f"Begin receiving MIDI data on channel {self._instrument_name}...")
             threading.Thread(target=self._read_midi_input, daemon=True).start()
 
     def stop(self):
         """Stops the channel and ends all active notes."""
         super().stop()  # Set the channel to "stop"
         if not self._is_playing:
-            logging.info(f"Stopping MIDI data listening on channel {self._instrument_name}...")
+            logging.debug(f"Stopping MIDI data listening on channel {self._instrument_name}...")
             for note in self._active_notes:
                 self._synth.noteoff(self._channel, note)
             self._active_notes.clear()
@@ -147,7 +153,7 @@ class FreeMidiChannel(InstrumentChannel):
     def _read_midi_input(self):
         """Reads MIDI inputs from a specific MIDI port and plays them."""
         with mido.open_input(self._port_name) as port:
-            logging.info(f"Listening for MIDI input on {self._port_name}...")
+            logging.debug(f"Listening for MIDI input on {self._port_name}...")
             for message in port:
                 if self._is_playing:
                     self._play_midi_message(message)
@@ -164,7 +170,7 @@ class FreeMetronomeChannel(InstrumentChannel):
         """Responds to the tick and plays a note with accent logic."""
         if self._is_playing:
             # Determine if this is the first beat of a measure
-            self._tick_count = (self._tick_count % self._project.get_beats_per_measure()) + 1
+            self._tick_count = (self._tick_count % self._parent.get_beats_per_measure()) + 1
 
             if self._tick_count == 1:
                 # Accent on the first beat of the measure (louder)
@@ -177,15 +183,19 @@ class FreeMetronomeChannel(InstrumentChannel):
 
             # Play the note for the duration of seconds_per_beat
             self._synth.noteon(self._channel, note, velocity)
-            time.sleep(self._project.get_seconds_per_beat())  # The duration of the note matches the length of a beat
+            time.sleep(self._parent.get_seconds_per_beat())  # The duration of the note matches the length of a beat
             self._synth.noteoff(self._channel, note)
 
 class StepSequencerChannel(InstrumentChannel):
     """A channel for a step sequencer."""
     
-    def __init__(self, project, sequencer, volume=80):
-        super().__init__(project, "", volume)
+    def __init__(self, project, sequencer, name, volume=80):
+        super().__init__(project, name, volume)
         self._sequencer = sequencer
+
+    def get_sequencer(self):
+        """Returns sequencer"""
+        return self._sequencer
 
     def play(self):
         """Starts the channel"""
@@ -202,12 +212,12 @@ class StepSequencerChannel(InstrumentChannel):
 class StepChannel:
     """Abstract base class for all step channels."""
     
-    def __init__(self, sequencer, instrument_name, volume=80):
-        self._sequencer = sequencer
+    def __init__(self, parent, instrument_name, volume=80):
+        self._sequencer = parent
         self._instrument_name = instrument_name
         self._volume = volume
         self._is_playing = False
-        self._steps = []
+        self._steps = [None] * self._sequencer.get_steps()
 
     def set_step(self, step, note, velocity):
         # If the step index is greater than the current list, extend the list with None
@@ -242,13 +252,13 @@ class StepChannel:
     def play(self):
         """Starts the channel."""
         if not self._is_playing:
-            logging.info(f"Starting Step Channel {self._instrument_name}...")
+            logging.debug(f"Starting Step Channel {self._instrument_name}...")
             self._is_playing = True
 
     def stop(self):
         """Stops the channel."""
         if self._is_playing:
-            logging.info(f"Pausing Step Channel {self._instrument_name}...")
+            logging.debug(f"Pausing Step Channel {self._instrument_name}...")
             self._is_playing = False
 
     def get_steps(self):
@@ -286,7 +296,10 @@ class StepSequencer:
         # Set new beat callback
         self._seq.timer(self._start_time + int(self._seconds_per_beat * 1000 * (self._cur_step + 1)), dest=self._step_callback_id)
 
-        logging.info(f"Beat {self._cur_step} triggered: time={time}")
+        logging.debug(f"Beat {self._cur_step} triggered: time={time}")
+
+    def get_steps(self):
+        return self._steps
 
     def add_channel(self, channel):
         """Adds a channel to the step sequencer."""
@@ -295,6 +308,10 @@ class StepSequencer:
     def remove_channel(self, channel):
         """Removes a channel from the step sequencer."""
         self._channels.remove(channel)
+
+    def get_channels(self):
+        """Returns all step sequencer channels"""
+        return self._channels
 
     def update(self):
         """Loads the notes of the step channels into the sequence"""
@@ -307,25 +324,26 @@ class StepSequencer:
                     note = step[0]                              # MIDI note value
                     velocity = step[1]                          # Note velocity
 
-                    # Set Note-On event
-                    self._seq.note_on(
-                        start_tick,            # Time of the Note-On event
-                        0,                     # MIDI channel (here channel 0, can be adjusted)
-                        note,                  # MIDI note value
-                        velocity,              # Note velocity
-                        dest=self._synth_id    # Target Synthesizer ID
-                    )
+                    if note and velocity:
+                        # Set Note-On event
+                        self._seq.note_on(
+                            start_tick,            # Time of the Note-On event
+                            0,                     # MIDI channel (here channel 0, can be adjusted)
+                            note,                  # MIDI note value
+                            velocity,              # Note velocity
+                            dest=self._synth_id    # Target Synthesizer ID
+                        )
 
-                    # Set Note-Off event (one beat later)
-                    stop_tick = self._start_time + int(self._seconds_per_beat * 1000 * (i + 1))  # Calculates the stop time of the step
-                    self._seq.note_off(
-                        stop_tick,             # Time of the Note-Off event
-                        0,                     # MIDI channel
-                        note,                  # MIDI note value
-                        80,                    # Velocity when releasing the note
-                        dest=self._synth_id    # Target Synthesizer ID
-                    )
-                    logging.debug(f"  note_on({start_tick}, 0, {note}, 100), note_off({stop_tick} , 0, 80, 100)")
+                        # Set Note-Off event (one beat later)
+                        stop_tick = self._start_time + int(self._seconds_per_beat * 1000 * (i + 1))  # Calculates the stop time of the step
+                        self._seq.note_off(
+                            stop_tick,             # Time of the Note-Off event
+                            0,                     # MIDI channel
+                            note,                  # MIDI note value
+                            80,                    # Velocity when releasing the note
+                            dest=self._synth_id    # Target Synthesizer ID
+                        )
+                        logging.debug(f"  note_on({start_tick}, 0, {note}, 100), note_off({stop_tick} , 0, 80, 100)")
                 i = i + 1
 
     def play(self):
@@ -374,6 +392,10 @@ class Project:
         """Removes a channel from the project."""
         self._channels.remove(channel)
 
+    def get_channels(self):
+        """Returns all channels"""
+        return self._channels
+
     def start_ticking(self):
         """Starts the beat management and notifies all channels."""
         self._is_playing = True
@@ -387,7 +409,7 @@ class Project:
 
     def play(self):
         """Starts the project and all channels."""
-        logging.info("Starting the project.")
+        logging.debug("Starting the project.")
         threading.Thread(target=self.start_ticking).start()
         for channel in self._channels:
             logging.debug("Starting channel: %s", channel)
@@ -395,11 +417,61 @@ class Project:
 
     def stop(self):
         """Stops the project and all channels."""
-        logging.info("Stopping the project.")
+        logging.debug("Stopping the project.")
         self._is_playing = False
         for channel in self._channels:
             logging.debug("Stopping channel: %s", channel)
             channel.stop()
+
+def render_curses(screen, project):
+    """Curses render function for threading"""
+    try:
+        while True:
+            screen.clear()
+            line = 0
+            for i, channel in enumerate(project.get_channels()):
+                # Show channel name
+                screen.addstr(line, 0, f"Channel {i + 1}: {channel._instrument_name}")
+                if isinstance(channel, StepSequencerChannel):
+                    # Get parent sequencer
+                    sequencer = channel.get_sequencer()
+
+                    # Get children step channels
+                    step_channels = sequencer.get_channels()
+
+                    # Show steps
+                    for step_channel in step_channels:
+                        line=line+1
+                        screen.addstr(line, 3, "[")
+                        for j, step in enumerate(step_channel.get_steps()):
+                            if step is None:
+                                # Show non set steps
+                                screen.addstr(line, j * 2 + 4, "o-")
+                            else:
+                                # Show set steps
+                                note, velocity = step
+                                if note is None or velocity is None:
+                                    screen.addstr(line, j * 2 + 4, "o-")
+                                else:
+                                    screen.addstr(line, j * 2 + 4, "x-")
+                        screen.addstr(line, 67, "]")
+                line=line+1
+            screen.refresh()  # Refresh screen
+            time.sleep(0.5)   # Refresh rate
+    except curses.error:
+        pass  # Ignore display errors
+    except Exception as e:
+        # Handle all other exceptions
+        logging.error(f"Unexpected error happened: {e}")
+        screen.addstr(10, 0, f"!!! Unexpected error happened: {e} !!!")
+        screen.refresh()
+        time.sleep(10)  
+
+
+def start_curses_thread(project):
+    def curses_thread(screen):
+        render_curses(screen, project)
+    threading.Thread(target=curses.wrapper, args=(curses_thread,), daemon=True).start()
 
 # Example Usage:
 
@@ -425,14 +497,6 @@ step_channel_1.set_step(5, 63, 127)
 step_channel_1.set_step(6, 62, 127)
 step_channel_1.set_step(7, 61, 127)
 step_channel_1.set_step(8, 60, 127)
-step_channel_1.set_step(9, 61, 127)
-step_channel_1.set_step(10, 62, 127)
-step_channel_1.set_step(11, 63, 127)
-step_channel_1.set_step(12, 64, 127)
-step_channel_1.set_step(13, 63, 127)
-step_channel_1.set_step(14, 62, 127)
-step_channel_1.set_step(15, 61, 127)
-step_channel_1.set_step(16, 60, 127)
 step_channel_1.set_step(17, 61, 127)
 step_channel_1.set_step(18, 62, 127)
 step_channel_1.set_step(19, 63, 127)
@@ -441,13 +505,6 @@ step_channel_1.set_step(21, 63, 127)
 step_channel_1.set_step(22, 62, 127)
 step_channel_1.set_step(23, 61, 127)
 step_channel_1.set_step(24, 60, 127)
-step_channel_1.set_step(25, 61, 127)
-step_channel_1.set_step(26, 62, 127)
-step_channel_1.set_step(27, 63, 127)
-step_channel_1.set_step(28, 64, 127)
-step_channel_1.set_step(29, 63, 127)
-step_channel_1.set_step(30, 62, 127)
-step_channel_1.set_step(31, 61, 127)
 
 # Assign Step Channels
 step_sequencer.add_channel(step_channel_1)
@@ -460,8 +517,11 @@ for port_name in mido.get_input_names():
 metronome_channel = FreeMetronomeChannel(project, "Metronome")
 project.add_channel(metronome_channel)
 
-step_sequencer_channel = StepSequencerChannel(project, step_sequencer)
+step_sequencer_channel = StepSequencerChannel(project, step_sequencer, "Step Sequencer")
 project.add_channel(step_sequencer_channel)
+
+# Start curses display
+start_curses_thread(project)
 
 # Start the project
 project.play()
